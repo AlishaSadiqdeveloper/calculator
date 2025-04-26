c1a805e5608b285e8d8d3dcbfde00aa021aa7203
AlishaSadiqdeveloper <alishasadiq21@gmail.com>
2025-04-26 15:57:08 +0500

    Add files via upload

---
+++ a.js
@@ -0,0 +1,19 @@
+// console.log(" This is working");
+let number1=parseInt(prompt("First number"));
+console.log(" First number");
+let number2 = parseInt(prompt("second number"));
+console.log(" second number");
+let sum = parseInt(number1+number2);
+console.log(sum);
+let sub = (number1-number2);
+console.log(sub);
+let multiply = (number1*number2);
+console.log(multiply);
+let divide = (number1/number2);
+console.log(divide);
+
+
+
+
+
+
